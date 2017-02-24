92abcfae5ed62b9e5ad07cf7830484449d90d1f8
Phipp <phip@boolean.party>
2017-02-24 00:16:26 -0500

    little tweaks plus

--- update.py
+++ update.py
@@ -17,7 +17,7 @@ def get_records():
     sheet = client.open("Keycards").sheet1
     return sheet.get_all_records(default_blank=None)
 
-def format_fobs(fobs):
+def format_fobs_by_id(fobs):
     return {i['ID'] : {"name": i['Name'], "maindoor": "authorized"} for i in fobs if not i['Disable?']}
 
 def find_duplicates(rows):
@@ -31,34 +31,32 @@ def find_duplicates(rows):
             logger.warn(id_dict[dup])
 
 if __name__ == "__main__":
-    # Logging
     global logger
     logger = logging.getLogger("Doorwoman-updater")
     logger.setLevel(logging.INFO)
     logger.addHandler(JournalHandler())
-    # logger.addHandler(logging.StreamHandler())
 
     users_file = open('conf/users.json', 'r') # This file MUST exist
     users_json = users_file.read()
     users = json.loads(users_json)
 
     records = get_records()
-    records_by_id = format_fobs(records)
+    records_by_id = format_fobs_by_id(records)
 
     find_duplicates(records)
-    logger.info("Read %i ids from users.json, Downloaded %i (%i enabled) ids from Spreadsheet." %
+    logger.debug("Read %i ids from users.json, Downloaded %i (%i enabled) ids from Spreadsheet." %
         (len(users), len(records), len(records_by_id)))
 
     records_by_id_json = json.dumps(records_by_id, indent=4)
     if records_by_id_json == users_json:
         exit()
-    elif len(users) and not len(records) or not len(records_by_id):
+    elif len(users) and not len(records_by_id):
         logger.warning("ERROR! Refusing to overwrite with zero users")
     else:
         logger.info("Updating conf/users.json: %i new records" % (len(records_by_id) - len(users)))
         users_file.close()
         call('./datamounter.sh -w'.split())
-        users_file.open('conf/users.json', 'r+')
+        users_file = open('conf/users.json', 'r+')
         json.dump(records_by_id, users_file, indent=4)
         users_file.truncate()
         users_file.close()
